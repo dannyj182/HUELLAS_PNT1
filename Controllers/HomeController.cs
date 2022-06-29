38cf0d11e9bf08d305805e134c51788c841032b3
Martina Pereyra <1martinapereyra@gmail.com>
2022-06-29 18:32:35 -0300

    agrego mascotas, contacto, y nosotros en Homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,58 @@
-﻿using HUELLAS_PNT1.Models;
+﻿using HUELLAS_PNT1.Context;
+using HUELLAS_PNT1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+
 namespace HUELLAS_PNT1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private readonly HuellasDatabaseContext _context;
+
+        public HomeController(ILogger<HomeController> logger, HuellasDatabaseContext context)
         {
             _logger = logger;
+            _context = context;
         }
+        
+ 
 
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+    
+        public async Task<IActionResult> Mascotas()
+        {
+            return View(await _context.Pets.ToListAsync());
+        }
+
+
+        public IActionResult Contacto()
+        {
+            var _misMascotas = new SelectList(_context.Pets.ToList(), "Id", "Nombre", null);
+            ViewBag.MisMascotas = _misMascotas;
+            return View();
+        }
+
+
 
         public IActionResult Privacy()
         {
